62314e544fb2af86c86ac156d2840db710edb64a
Szymon Wójcik <szyper7@gmail.com>
2024-03-26 10:22:48 +0100

    Initializing next & react github pages website

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+    basePath: "/wrss-wi-strona",
+};
+
+export default nextConfig;
